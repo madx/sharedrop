614c2fb4c097a699530dfb27558d54f4c192183d
madx <madx+github@yapok.org>
2013-09-25 12:52:20 +0200

    Add file listing + thumbnailing

--- sharedrop.sh
+++ sharedrop.sh
@@ -4,6 +4,7 @@ CONFIG_DIR=${XDG_CONFIG_HOME:-$HOME/.config}/sharedrop
 DATA_DIR=${XDG_DATA_HOME:-$HOME/.local/share}/sharedrop
 LOGFILE=${LOGFILE:-$HOME/}
 CONFIG_FILE="$CONFIG_DIR/config.sh"
+INDEX_FILE_NAME="list.html"
 
 log() {
   echo "`date +%FT%T` $@"
@@ -27,7 +28,45 @@ error () {
   die
 }
 
+build_index () {
+  INDEX_FILE="$DATA_DIR/files/$INDEX_FILE_NAME"
+
+  echo -n > "$INDEX_FILE"
+
+  ls -t1 "$DATA_DIR/files" | egrep -v "$INDEX_FILE_NAME|thumbs" | while read file; do
+    ext="${file##*.}"
+
+    case "$ext" in
+      png|jpg|jpeg|gif)
+        thumbfile="$DATA_DIR/files/thumbs/$file"
+
+        if [ ! -f "$thumbfile" ]; then
+          log "Generating thumbnail for $file"
+          convert -resize 100x100 "$DATA_DIR/files/$file" "$thumbfile"
+        fi
+
+        thumb="<img src=\"thumbs/$file\" />"
+        ;;
+      *|'')
+        thumbfile="$DATA_DIR/files/thumbs/$ext.png"
+
+        if [ ! -f "$thumbfile" ]; then
+          log "Generating thumbnail for $ext"
+          convert -size 100x100 -gravity center -pointsize 32 "label:$ext" "$thumbfile"
+        fi
+
+        thumb="<img src=\"thumbs/$ext.png\" />"
+        ;;
+    esac
+
+    echo "<a href=\"$file\">$thumb</a>" >> "$INDEX_FILE"
+  done
+}
+
 sync () {
+  log "Syncing"
+  build_index
+
   rsync -e ssh -Lavz --chmod=ug=rX,o=rX --delete-after "$DATA_DIR/files/" "$REMOTE" 2>&1 |
     while read line; do
       log "rsync: $line"
@@ -54,7 +93,7 @@ if [ ! -d $CONFIG_DIR ]; then
 fi
 if [ ! -d $DATA_DIR ]; then
   log "Creating data dir ($DATA_DIR)"
-  mkdir -p $DATA_DIR/files/ # Also automatically create the files folder
+  mkdir -p $DATA_DIR/files/thumbs # Also automatically create the files and thumbs folder
 fi
 
 # Setup
